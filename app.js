4a193801a9f9d3e41bf6dde4a5c9e9d1ae26820e
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2016-01-26 14:24:06 +0100

    Step 1

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 'use strict';
 
-var hippoRestApp = angular.module('hippoRestApp', []);
+var hippoRestApp = angular.module('hippoRestApp', [ 'ngRoute', 'ngResource' ]);
 
-hippoRestApp.controller('DocumentsController', function($scope) {
-  $scope.message = "Nothing here yet";
+hippoRestApp.constant('apiPrefix', 'http://localhost:8080/site/api/');
+
+hippoRestApp.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl : 'document-list.html',
+    controller : 'DocumentsController'
+  }).otherwise('/');
+});
+
+hippoRestApp.factory('DocumentsService', function($resource, apiPrefix) {
+  return {
+    getList : function() {
+      return $resource(apiPrefix + 'documents/', {}).get();
+    }
+  }
+});
+
+hippoRestApp.controller('DocumentsController', function($scope,
+    DocumentsService, apiPrefix) {
+
+  DocumentsService.getList().$promise.then(function(response) {
+    $scope.documents = response;
+  });
+
 });
