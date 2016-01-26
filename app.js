f271fa5a9654e96e05dea4c8214daab3d808ce94
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2016-01-26 14:16:45 +0100

    Step 0

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var hippoRestApp = angular.module('hippoRestApp', []);
+
+hippoRestApp.controller('DocumentsController', function($scope) {
+  $scope.message = "Nothing here yet";
+});
