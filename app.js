1ee374fff720984b0eb45c88da442dd5a75a3ff3
Niels van Kampenhout <n.vankampenhout@onehippo.com>
2016-02-17 14:43:59 -0800

    change line wrapping from 80 to 120

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var hippoRestApp = angular.module('hippoRestApp', [ 'ngRoute', 'ngResource',
-    'ngSanitize' ]);
+var hippoRestApp = angular.module('hippoRestApp', [ 'ngRoute', 'ngResource', 'ngSanitize' ]);
 
 hippoRestApp.constant('apiPrefix', 'http://localhost:8080/site/api/');
 
@@ -28,8 +27,7 @@ hippoRestApp.factory('DocumentsService', function($resource, apiPrefix) {
   }
 });
 
-hippoRestApp.controller('DocumentsController', function($scope, $routeParams,
-    DocumentsService, apiPrefix) {
+hippoRestApp.controller('DocumentsController', function($scope, $routeParams, DocumentsService, apiPrefix) {
 
   if (!$routeParams.uuid) {
 
@@ -48,8 +46,7 @@ hippoRestApp.controller('DocumentsController', function($scope, $routeParams,
     }
 
   } else {
-    DocumentsService.getDocumentById($routeParams.uuid).$promise.then(function(
-        response) {
+    DocumentsService.getDocumentById($routeParams.uuid).$promise.then(function(response) {
       $scope.document = response;
 
       // resolve internal links
@@ -66,10 +63,8 @@ hippoRestApp.controller('DocumentsController', function($scope, $routeParams,
     someElement.innerHTML = response.items['myhippoproject:content'].content;
     var links = someElement.querySelectorAll('a[data-hippo-link]');
     for (var index = 0; index < links.length; index++) {
-      if (response.items['myhippoproject:content'].links[links[index]
-          .getAttribute('data-hippo-link')]) {
-        var uuid = response.items['myhippoproject:content'].links[links[index]
-            .getAttribute('data-hippo-link')].id;
+      if (response.items['myhippoproject:content'].links[links[index].getAttribute('data-hippo-link')]) {
+        var uuid = response.items['myhippoproject:content'].links[links[index].getAttribute('data-hippo-link')].id;
         links[index].href = '#/' + uuid;
       }
     }
